6b3b74940a02b77d87bdf80425f501831e1f9e94
Meer Nasser Ali <meernasser.ali@cognizant.com>
2017-10-06 15:38:37 +0530

    Learned
    1. Routing using Convention-based (Default and Custom
    2. Routing using Attributes - Route, RoutePrefix, HTTP Methods
    3. API Response using - HttpResponseMessage and IHttpActionResult

---
+++ LearnWebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,45 @@
+﻿using LearnWebAPI.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
+
+namespace LearnWebAPI.Controllers
+{
+    [RoutePrefix("api/orders")]
+    public class OrdersController : ApiController
+    {
+        Order[] orders = new Order[]
+        {
+            new Order { OrderID = "1_1", SourceOrderID = 1, SKU = "1342561", SKU_Desc = "Cozy by the Fire™", Quantity = 1, Image = "http://www.yankeecandle.com/media/images/product/small/1342561.jpg" },
+            new Order { OrderID = "1_2", SourceOrderID = 1, SKU = "1187884", SKU_Desc = "Apple Cider", Quantity = 3, Image = "http://www.yankeecandle.com/media/images/product/small/1187884.jpg" },
+            new Order { OrderID = "2_1", SourceOrderID = 2, SKU = "1241216", SKU_Desc = "Bahama Breeze™", Quantity = 1, Image = "http://www.yankeecandle.com/media/images/product/small/1241216.jpg" },
+            new Order { OrderID = "2_2", SourceOrderID = 2, SKU = "1510124", SKU_Desc = "Crisp Fall Night", Quantity = 1, Image = "http://www.yankeecandle.com/media/images/product/small/1510124.jpg" }
+        };
+
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return orders;
+        }
+
+        [Route("{order_id}", Name = "GetOrderByOrderID")]
+        public HttpResponseMessage GetOrderByOrderID(string order_id) {
+            var orderList = orders.Where((o) => o.OrderID == order_id);
+
+            // Write the list to the response body.
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, orderList);
+            return response;
+
+        }
+
+        [Route("{order_id:int}", Name = "GetOrderBySourceOrderID")]
+        public IHttpActionResult GetOrderBySourceOrderID(int order_id)
+        {
+            var orderList = orders.Where((o) => o.SourceOrderID == order_id);
+            return Ok(orderList);
+        }
+    }
+}
---
+++ LearnWebAPI/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnWebAPI.Models
+{
+    public class Order
+    {
+        public string OrderID { get; set; }
+        public int SourceOrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string SKU { get; set; }
+        public string SKU_Desc { get; set; }
+        public int Quantity { get; set; } 
+        public string Image { get; set; }
+    }
+}
